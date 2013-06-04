400b2fe927c30209b25cb64b89ed578b02b8d0d7
tulioFcastro <tulio.figueiredo@gmail.com>
2013-06-03 21:40:06 -0700

    Tudo

---
+++ lab1/src/ufcg/si1/Teste.java
@@ -0,0 +1,12 @@
+package ufcg.si1;
+
+public class Teste {
+
+	public static String xpto(){
+		return "Lab01-SI1";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(xpto());
+	}
+}
---
+++ lab1/src/ufcg/si1/TesteTest.java
@@ -0,0 +1,14 @@
+package ufcg.si1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteTest {
+
+	@Test
+	public void testXpto() {
+		assertEquals("Lab01-SI1", Teste.xpto());
+	}
+
+}
